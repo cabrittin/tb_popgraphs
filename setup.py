ce64e7d133682d1d9d1babec48257bbe2d4b7023
Christopher Brittin <cabrittin@gmail.com>
2024-06-26 12:50:46 -0400

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='tb_popgraphs',
+      version='1.0',
+      description='General use tool box',
+      author='cabrittin',
+      author_email='cabrittin at the google',
+      url='git@github.com:cabrittin/tb_popgraph.git',
+      packages=['tb_popgraphs','tb_popgraphs.generators','tb_popgraphs.graphs'],
+     )
